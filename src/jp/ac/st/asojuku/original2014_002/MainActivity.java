00c356fef8639405ad907651f158a9d8164a6d53
ASO1201780 <1201780@st.asojku.ac.jp>
2014-06-18 15:26:56 +0900

    2014/6/18

--- src/jp/ac/st/asojuku/original2014_002/MainActivity.java
+++ src/jp/ac/st/asojuku/original2014_002/MainActivity.java
@@ -1,22 +1,52 @@
 package jp.ac.st.asojuku.original2014_002;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import android.app.Activity;
-import android.view.Menu;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 
-public class MainActivity extends Activity {
-
-	@Override
+public class MainActivity extends Activity implements View.OnClickListener {
+	
+	SQLiteDatabase sdb = null;
+	MySQLiteOpenHelper helper = null;
+	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
-
+	
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+		Button btnENTRY = (Button)findviewByid(R.id.btnENTRY);
+		btnENTRY.setOnClickListener(this);
+		
+		Button btnMAINTE = (Button)findViewByid(R.id.btnMAINTE);
+		btnMAINTE.setOnClickListener
+		
+		Button btnCHECK = (Button)findViewByid(R.id.btnCHECK);
+		btnCHECK.setOnClickListener(this);
 	}
-
+	if(sdb == null) {
+			helper = new MySQLiteOpenHelper(getApplicatContext());			
+	}
+	try{
+		sdb = helper.getWritableDatabase();
+	}catch(SQLiteException e){
+			
+		return;		
+		}
+	}
+	@Override
+	public void onClick(View v) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		
+		
+	}
+		
 }
--- src/jp/ac/st/asojuku/original2014_002/MySQLiteOpenHelper.java
+++ src/jp/ac/st/asojuku/original2014_002/MySQLiteOpenHelper.java
@@ -1,11 +1,14 @@
 package jp.ac.st.asojuku.original2014_002;
 
-import android.datebase.sqlite.SQLiteOpenHelper;
-import android.os.Bundle;
-import android.datebase.sqlite.SQLiteDatebase;
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteCursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class MySQLiteOpenHelper extends SQLiteOpenHelper {
-	
+
 	/**
 	 * @param context　呼び出しコンテクスト
 	 * @param name 利用DB名
@@ -13,20 +16,66 @@ public class MySQLiteOpenHelper extends SQLiteOpenHelper {
 	 * @param version DSバージョン
 	 * */
 	public MySQLiteOpenHelper(Context context) {
-	
+
 		super(context,"20140021201711.sqlite3",null,1);
-		
+
 	}
 	@Override
-	public void onCreate(SQLiteDatebase db) {
+	public void onCreate(SQLiteDatabase db) {
 		// TOOD 自動生成されたメソッド・スタブ
 		db.execSQL("CREATE TABLE IF NOT EXISTS"+
 					"Hitokoto (_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL ,phrase TEXT )");
 	}
-	
+
 	@Override
-	public onUpgrade(SQLiteDatebase db, int oldVersion int newVersion) {
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		db.execSQL("drop table Hitokoto");
 		onCreate(db);
 	}
 
+	/**
+	 *  引数のフレーズをHitokotoテーブルにインサートするプライベートメソッド
+	 *  @param SQLiteDatabase インサート先ＤＢのインスタンス変数
+	 *  @inputMsg インサートするメッセージ
+	 */
+	public void insertHitokoto(SQLiteDatabase db, String inputMsg){
+
+		String sqlstr = "insert into Hitokoto (phrase) values(' " + inputMsg + " '); ";
+				try {
+					//トランザクション開始
+					db.beginTransaction();
+					db.execSQL(sqlstr);
+					//トランザクション成功
+					db.setTransactionSuccessful();
+				} catch (SQLException e) {
+					Log.e("ERROR", e.toString());
+				}finally{
+						//トランザクション終了
+						db.endTransaction();
+					}
+				return;
+
+			}
+				public String selectRandomHitokoto(SQLiteDatabase db){
+
+					String rtString = null;
+
+					String sqlstr = "SELECT _id, phrase FROM Hitokoto ORDER BY RANDOM();";
+					try{
+					SQLiteCursor cursor = (SQLiteCursor)db.rawQuery(sqlstr,  null);
+					if(cursor.getCount()!=0){
+						//カーソル開始位置を先頭にする
+						cursor.moveToFirst();
+						rtString = cursor.getString(1);
+					}
+						cursor.close();
+
+					}catch (SQLException e) {
+						Log.e("ERROR", e.toString());
+					}finally {
+
+					}
+			return rtString;
+					}
+
+				}
